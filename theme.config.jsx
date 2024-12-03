cdc6864ae8552ec97cdc842e7db8e2d0f7fab992
Eason Li <lyu63651@gmail.com>
2024-12-03 08:36:17 +0800

    new social card

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,5 @@
 export default {
-  docsRepositoryBase:
-    "https://github.com/eli32-vlc/fmhyguidesindex/tree/main",
+  docsRepositoryBase: "https://github.com/eli32-vlc/fmhyguidesindex/tree/main",
   logo: (
     <span>
       <strong>FMHY Guides Index</strong>
@@ -19,14 +18,14 @@ export default {
       />
       <meta
         property="og:image"
-        content="https://small.fileditchstuff.me/s19/YsciPpCiHZrbQvbTrLl.png"
+        content="https://small.fileditchstuff.me/s19/pFnpOOcgngakswoMlxg.png"
       />
       <meta property="og:image:alt" content="FMHY Guides Index Social Card" />
       <meta property="og:url" content="https://guides.fmhy.lol" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta
         name="twitter:image"
-        content="https://small.fileditchstuff.me/s19/YsciPpCiHZrbQvbTrLl.png"
+        content="https://small.fileditchstuff.me/s19/pFnpOOcgngakswoMlxg.png"
       />
       <meta
         name="twitter:description"
